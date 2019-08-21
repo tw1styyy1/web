0d2a0a76c2fb21ccf373336b1c7eeec8b4dd7e14
Kirill <arkipovkirill43@gmail.com>
2019-08-22 00:12:44 +0300

    Это наш первый коммит

---
+++ main.js
@@ -0,0 +1,12 @@
+var obj = {};
+obj = {
+    name:'', 
+    arh:''
+};
+
+
+obj['name'] = prompt('Ваше имя?');
+obj['arh'] = prompt('Сколько вам полных лет?');
+
+console.log(obj['name']);
+console.log(obj['arh']);
